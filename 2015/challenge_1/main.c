ecc6b9e0592e8471b716c0757f19632e3b8e7ad1
Richard Sullivan <richardsullivan@Richards-MBP.lan>
2024-08-21 12:33:57 -0500

    feat: solve challenge1 part 1

--- 2015/challenge_1/main.c
+++ 2015/challenge_1/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
-  printf("Hello, World\n");
+  // open file
+  FILE* file = fopen("input.txt", "r");
+
+  // process file
+  int counter = 0;
+  char c;
+  while ((c = fgetc(file)) != EOF) {
+    if ( c == '(' ) {
+      counter++;
+    }
+    else if ( c == ')' ) {
+      counter--;
+    }
+  }
+  // close file
+  fclose(file);
+
+  // print result
+  printf("%d\n",counter);
   return 0;
 }
