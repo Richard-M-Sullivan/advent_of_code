422c7441c278733d37eee1ba1397469444959073
Richard Sullivan <richardsullivan@Richards-MBP.lan>
2024-08-27 10:11:15 -0500

    feat:implement part 2 function

--- 2015/challenge_1/main.c
+++ 2015/challenge_1/main.c
@@ -1,24 +1,64 @@
 #include <stdio.h>
 
+int part1(char* input_file);
+int part2(char* input_file);
+
 int main(int argc, char** argv){
+  
+  printf("The answer to part 1 is: %d\n",part1("input.txt"));
+  printf("The answer to part 2 is: %d\n",part2("input.txt"));
+  return 0;
+}
+
+// get final floor after going through entire input
+int part1(char* input_file){
   // open file
-  FILE* file = fopen("input.txt", "r");
+  FILE* file = fopen(input_file, "r");
 
   // process file
-  int counter = 0;
+  int floor = 0;
   char c;
   while ((c = fgetc(file)) != EOF) {
     if ( c == '(' ) {
-      counter++;
+      floor++;
     }
     else if ( c == ')' ) {
-      counter--;
+      floor--;
     }
   }
   // close file
   fclose(file);
 
-  // print result
-  printf("%d\n",counter);
-  return 0;
+  // return result
+  return floor;
+}
+
+// get the position in the file where floor is negative 1
+int part2(char* input_file){
+  // open file
+  FILE* file = fopen(input_file, "r");
+
+  // process file
+  int counter = 0;
+  int floor = 0;
+  char c;
+  while ((c = fgetc(file)) != EOF) {
+    if ( c == '(' ) {
+      counter++;
+      floor++;
+    }
+    else if ( c == ')' ) {
+      counter++;
+      floor--;
+    }
+
+    if ( floor < 0 ) {
+      break;
+    }
+  }
+  // close file
+  fclose(file);
+
+  // return result
+  return counter;
 }
