b5751ad7e197f4590e1f39d90020fdfd1d0def00
Richard Sullivan <richardsullivan@Richards-MBP.lan>
2024-08-27 10:59:37 -0500

    feat: add box_sides struct and basic setter

--- 2015/challenge_2/main.c
+++ 2015/challenge_2/main.c
@@ -4,15 +4,24 @@ typedef struct box_sides{
   int x, y, z;
 } box_sides;
 
+// takes in a string, and sets the values in box
+// returns 0 if no error, and 1 if an error
+int set_box_from_line(box_sides* box, char* line);
+
 int part1(char* input_file);
 int part2(char* input_file);
-
 
 int main(int argc, char** argv){
   
   //printf("The answer to part 1 is: %d\n",part1("input.txt"));
   //printf("The answer to part 2 is: %d\n",part2("input.txt"));
-  
+  char* line = "hello";
+  box_sides box = {1, 1, 1};
+
+  set_box_from_line(&box, line);
+
+  printf("box is: x=%d, y=%d, z=%d.\n", box.x, box.y, box.z);
+
   printf("The answer to part 1 is: %s\n","TODO");
   printf("The answer to part 2 is: %s\n","TODO");
   
@@ -70,4 +79,12 @@ int part2(char* input_file){
 
   // return result
   return counter;
+}
+
+int set_box_from_line(box_sides* box, char* line) {
+  box->x = 0;
+  box->y = 0;
+  box->z = 0;
+
+  return 0;
 }
